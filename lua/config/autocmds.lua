65add4e2480bf8bd523d970b9886d57d02182f3c
pm <p.s.u.mannes@gmail.com>
2023-12-15 00:43:05 +0100

    ...

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true }
+map(
+  "n",
+  "<leader>ff",
+  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+  default_opts
+)
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,16 @@
+return {
+  -- { "shaunsingh/nord.nvim" },
+  -- { "bluz71/vim-moonfly-colors" },
+  -- { "helbing/aura.nvim" },
+  { "catppuccin/nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "nord",
+      -- colorscheme = "moonfly",
+      -- colorscheme = "aura",
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
