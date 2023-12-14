7345efb7c843a7309b8a628c12dde9d6a985e289
pm <p.s.u.mannes@gmail.com>
2023-12-15 00:58:22 +0100

    .

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.expandtab = false
+local opt = vim.opt
+opt.expandtab = false
+opt.grepprg = "rg --vimgrep --hidden"
